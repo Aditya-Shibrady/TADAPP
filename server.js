2519c7c05efc5b76ca56505381ecfe33c5982fb2
Aditya Shibrady <aditya.shibrady@gmail.com>
2015-12-16 23:31:30 -0600

    Push 2

--- server.js
+++ server.js
@@ -80,21 +80,24 @@ var success = function (data) {
     console.log('Data [%s]', data);
 };
 //
-//var Twitter = require('twitter-node-client').Twitter;
-//
-////Get this data from your twitter apps dashboard
-//var config = {
-//    "consumerKey": "pUhmj3kdqxjAen82RD5BX9kHE",
-//    "consumerSecret": "2QxB7RiNdJhXu4mvBXdLqotjgesbcpMJW52KwUsw9kqhIhjo0z",
-//    "accessToken": "105777356-cj3a7NY9BL4UGDpWz2ONvaL7f7IXUXMpG7qKTgm6",
-//    "accessTokenSecret": "KwckLghzxKLd4NqzHcOTocU0gaqm7P5B2eyWCgW3NrZk5"
-//}
-//
-//var twitter = new Twitter(config);
-//
-////Example calls
-//
-//twitter.getUserTimeline({ screen_name: 'aditya_shibrady', count: '10'}, error, success);
+var Twitter = require('twitter-node-client').Twitter;
+
+//Get this data from your twitter apps dashboard
+var config = {
+    "consumerKey": "pUhmj3kdqxjAen82RD5BX9kHE",
+    "consumerSecret": "2QxB7RiNdJhXu4mvBXdLqotjgesbcpMJW52KwUsw9kqhIhjo0z",
+    "accessToken": "105777356-cj3a7NY9BL4UGDpWz2ONvaL7f7IXUXMpG7qKTgm6",
+    "accessTokenSecret": "KwckLghzxKLd4NqzHcOTocU0gaqm7P5B2eyWCgW3NrZk5"
+}
+
+var twitter = new Twitter(config);
+
+//Example calls
+
+twitter.getUserTimeline({ screen_name: 'aditya_shibrady', count: '2'}, error, success);
+console.log("\n");
+console.log(".......................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................");
+//twitter.getUserTimeline({ screen_name: 'davemo2012', count: '1'}, error, success);
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
